6964686dc8a7bf4c6bc6baf67ba4ec3b6496c726
Enver Eren <erenenver@outlook.com>
2023-07-15 10:23:59 +0200

    Update Jhat.m

--- Jhat.m
+++ Jhat.m
@@ -2,7 +2,7 @@ function j_hat = Jhat(u,th,uh,h,rho,c,p,q,bheta,upper_bound_for_population)
     N = length(th); % Nh+1
     s1 = 0;
     s2 = 0;
-    for i = 0:N-2
+    for i = 1:N-1
         s1 = s1+ exp(-rho*th(i+1)) * (c - p * q *uh(i+1)) * u(i+1);
     end
     for i = 0:N-2
